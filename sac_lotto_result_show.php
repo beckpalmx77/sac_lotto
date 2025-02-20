8484224d141ec8bf9494639225c071cbf9d7a5d2
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-02-20 16:36:33 +0700

    Update sac_lotto_result_show.php

--- sac_lotto_result_show.php
+++ sac_lotto_result_show.php
@@ -8,6 +8,7 @@
     <title>ตรวจรางวัล</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body id="page-top">
 <div id="wrapper">
