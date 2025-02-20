952331e6f5d87fc464195e05104718a06c80e33e
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-02-20 08:28:00 +0700

    Update Header.php

--- includes/Header.php
+++ includes/Header.php
@@ -1,42 +1,37 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="ระบบจัดการข้อมูลรถยนต์ สงวนออโต้คาร์">
+    <meta name="author" content="Sanguan Auto Car">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
-    <link href="../img/logo/logo.png" rel="icon">
+
     <title>สงวนออโต้คาร์ | SANGUAN AUTO CAR</title>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="../js/jquery-3.5.1.min.js"></script>
+    <!-- ✅ FontAwesome (เลือกใช้เวอร์ชันเดียว) -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!-- ✅ Bootstrap 5 -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- ✅ jQuery (เลือกใช้เวอร์ชันเดียว) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- ✅ CSS สำหรับระบบ -->
     <link href="css/myadmin.css" rel="stylesheet">
-
     <link rel="stylesheet" href="vendor/alertify/css/alertify.core.css"/>
     <link rel="stylesheet" href="vendor/alertify/css/alertify.default.css"/>
     <link rel="stylesheet" href="vendor/alertify/css/main.css"/>
-
-    <!--script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script-->
-
-    <script src="vendor/alertify/js/alertify.js"></script>
-
     <link href="../css/select2_4010.css" rel="stylesheet"/>
+    <link href="../vendor/date-picker-1.9/css/date_picker_style.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="../css/datatables1115.css">
+
+    <!-- ✅ JavaScript -->
+    <script src="vendor/alertify/js/alertify.js"></script>
     <script src="../js/select2_4010.js"></script>
-
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
-    <link href="../vendor/datatables/v11/fontawesome53.all.min.css" rel="stylesheet">
-
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
-
     <script src="../vendor/date-picker-1.9/js/bootstrap-datepicker.js"></script>
     <script src="../vendor/date-picker-1.9/locales/bootstrap-datepicker.th.min.js"></script>
-    <link href="../vendor/date-picker-1.9/css/date_picker_style.css" rel="stylesheet"/>
-
-    <link rel="stylesheet" type="text/css" href="../css/datatables1115.css">
     <script type="text/javascript" charset="utf8" src="../js/datatables1115.js"></script>
 
     <?php
@@ -49,13 +44,4 @@
             font-family: 'Prompt', sans-serif !important;
         }
     </style>
-
-    <style>
-
-        th {
-
-        }
-
-    </style>
-
 </head>
