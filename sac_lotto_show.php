f4945250f2ecc932c002977511b549f586e1a53f
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-03-19 09:35:59 +0700

    Update sac_lotto_show.php

--- sac_lotto_show.php
+++ sac_lotto_show.php
@@ -98,7 +98,7 @@ require_once 'config/connect_lotto_db.php';
 <!-- Modal -->
 <div class="modal fade" id="updateModal" name="updateModal" tabindex="-1" aria-labelledby="updateModalLabel"
      aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="updateModalLabel">Update Record</h5>
